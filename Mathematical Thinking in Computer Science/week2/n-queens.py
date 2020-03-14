6b8a1815412fdd37590f4fe20ed87a7d8b3d3a34
toxoxi <yorori.too@gmail.com>
2020-03-14 12:02:44 +0900

    fix indent

--- Mathematical Thinking in Computer Science/week2/n-queens.py
+++ Mathematical Thinking in Computer Science/week2/n-queens.py
@@ -1,26 +1,26 @@
 # Example solution from the course
 
 def can_be_extended_to_solution(perm):
-    i = len(perm) - 1
-    for j in range(i):
-        if i - j == abs(perm[i] - perm[j]):
-            return False
-    return True
+	i = len(perm) - 1
+	for j in range(i):
+		if i - j == abs(perm[i] - perm[j]):
+			return False
+	return True
 
 # recursively main function
 # extend permutation if can
 def extend(perm, n):
-    if len(perm) == n:
-        print(perm)
-        exit()
+	if len(perm) == n:
+		print(perm)
+		exit()
 
-    for k in range(n):
-        if k not in perm: # deny duplication of number
-            perm.append(k)
+	for k in range(n):
+		if k not in perm: # deny duplication of number
+			perm.append(k)
 
-            if can_be_extended_to_solution(perm):
-                extend(perm, n)
+			if can_be_extended_to_solution(perm):
+				extend(perm, n)
 
-            perm.pop()
+			perm.pop()
 
 extend(perm = [], n = 20)
