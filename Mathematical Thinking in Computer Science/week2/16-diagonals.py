482e65cd18473ea4730c20a20a0db4e998294446
toxoxi <yorori.too@gmail.com>
2020-03-14 09:33:01 +0900

    solve 16-diagonals

--- Mathematical Thinking in Computer Science/week2/16-diagonals.py
+++ Mathematical Thinking in Computer Science/week2/16-diagonals.py
@@ -14,86 +14,74 @@ NONE = 0
 LT_TO_RB = 1
 RT_TO_LB = 2
 
-INVALID_DIAGONAL_INDICES_FOR_LT_TO_RB = {
-  LT_TO_RB: [[-1, 1], [1, -1]],
-  RT_TO_LB: [[0, -1], [-1, 0], [1, 0], [0, 1]],
-}
-
-INVALID_DIAGONAL_INDICES_FOR_RT_TO_LB = {
-  LT_TO_RB: [[0, -1], [-1, 0], [1, 0], [0, 1]],
-  RT_TO_LB: [[-1, -1], [1, 1]],
-}
-
-def execute(perm):
-  # diagonals = n * (n - 3) / 2 # n=5 → 16
-  # max_empty_cells = (n * n) - diagonals
-
-  fill_permutations(perm)
-  extend(perm, i = 0, j = 0)
-
 # fill permutations with UNDETERMINED
-def fill_permutations(perm):
-  for i in range(GRID_SIZE):
-    columns = []
-    for _ in range(GRID_SIZE):
-      columns.append(UNDETERMINED)
-
-    perm.append(columns)
+def filled_permutations():
+  perm=[]
+  for i in range(GRID_SIZE * GRID_SIZE):
+    perm.append(UNDETERMINED)
+  return perm
 
 # extend permutation from bottom to top, from left to right
 # 
 # perm: comprehensive permutation
-# n: number of cells of a side
-# i: current horizontal index
-# j: current vertical index
-def extend(perm, i, j):
-    if i >= GRID_SIZE:
-      print(perm)
-      exit()
+def extend(perm):
+  if perm[len(perm)-1] != UNDETERMINED:
+    print(perm)
+    exit()
 
-    for diagonal in [LT_TO_RB, RT_TO_LB, NONE]:
-      perm[i][j] = diagonal
+  last_index = find_last_determined_index(perm)
+  current_index = last_index + 1
 
-      if can_be_extended_to_solution(perm, i, j, diagonal):
-        # go to next column if vertical index reached to the top
-        if j == GRID_SIZE - 1:
-          j = 0
-          i += 1
-        else:
-          j += 1
+  for diagonal in [LT_TO_RB, RT_TO_LB, NONE]:
+    perm[current_index] = diagonal
+    if can_be_extended_to_solution(perm, current_index):
+      extend(perm)
+    
+  perm[current_index] = UNDETERMINED
 
-        extend(perm, i, j)
-      
-def can_be_extended_to_solution(perm, i, j, diagonal):
-  surround_range = range(-1, 2)
-  
-  if diagonal == LT_TO_RB:    
-    for type in [LT_TO_RB, RT_TO_LB]:
-      for indices in INVALID_DIAGONAL_INDICES_FOR_LT_TO_RB[type]:
-        target_i = i + indices[0]
-        target_j = j + indices[1]
-        if is_over_grid(target_i, target_j):
-          continue
-        if perm[target_i][target_j] == type:
-          return False
+def find_last_determined_index(perm):
+  for i, diagonal in enumerate(perm):
+    if diagonal == UNDETERMINED:
+      return i-1
 
-  elif diagonal == RT_TO_LB:
-    for type in [LT_TO_RB, RT_TO_LB]:
-      for indices in INVALID_DIAGONAL_INDICES_FOR_RT_TO_LB[type]:
-        target_i = i + indices[0]
-        target_j = j + indices[1]
-        if is_over_grid(target_i, target_j):
-          continue
-        if perm[target_i][target_j] == type:
-          return False
+def can_be_extended_to_solution(perm, i):
+  max_empty_cells = 9 # better to be calculated based on GRID_SIZE
 
-  else:
-    return True
-
-def is_over_grid(i, j):
-  if ((i < 0) or (i >= GRID_SIZE)) or ((j < 0) or (j >= GRID_SIZE)):
-    return True
-  else:
+  # false if no possibility
+  if perm.count(NONE) > max_empty_cells:
     return False
 
-execute(perm = [])
+  # next if NONE
+  if perm[i] == NONE:
+    return True
+
+  n = GRID_SIZE
+  is_top = (i + 1) % n == 0
+  is_bottom = (i + 1) % n == 1
+  is_left = i < n
+  opposite_diagonal = RT_TO_LB if perm[i] == LT_TO_RB else LT_TO_RB
+
+  diffs = []
+  if not is_left:
+    diffs.append(-n)
+  if not is_left and not is_bottom:
+    diffs.append(-n-1)
+  if not is_left and not is_top:
+    diffs.append(-n+1)
+  if not is_bottom:
+    diffs.append(-1)
+
+  for d in diffs:
+    target_index = i + d
+    compare_target = perm[target_index]
+    if abs(d) == 1 or abs(d) == n:
+      if compare_target == opposite_diagonal:
+        return False
+    else:
+      if (perm[i] == LT_TO_RB and abs(d) == n-1) or (perm[i] == RT_TO_LB and abs(d) == n+1):
+        if compare_target == perm[i]:
+          return False
+
+  return True
+
+extend(perm = filled_permutations())
